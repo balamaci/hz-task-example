c7a449f03a83a3a7bc92a7fccd94349169e0158f
Serban Balamaci <balamaci@gmail.com>
2014-07-22 10:35:48 +0300

    inject hz config in ClusterMaster constructor

--- src/main/java/ro/fortsoft/hztest/master/StartTestMaster.java
+++ src/main/java/ro/fortsoft/hztest/master/StartTestMaster.java
@@ -9,6 +9,7 @@ import ro.fortsoft.hztask.master.listener.TaskCompletionHandlerFactory;
 import ro.fortsoft.hztest.master.handler.GetWebPageTaskCompletionHandler;
 import ro.fortsoft.hztest.task.EchoTask;
 import ro.fortsoft.hztest.task.GetWebPageTask;
+import com.hazelcast.config.Config;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -31,11 +32,12 @@ public class StartTestMaster {
                     return new GetWebPageTaskCompletionHandler();
                 }
             });
-            ClusterMaster clusterMaster = new ClusterMaster(masterConfig, "hzMaster.xml");
+            Config hazelcastConfig = new ClasspathXmlConfig(configXmlFileName);
+            ClusterMaster clusterMaster = new ClusterMaster(masterConfig, hazelcastConfig);
 
             log.info("Submitting a distributed task");
-//            clusterMaster.submitTask(new GetWebPageTask("https://www.facebook.com"));
-            addEchoTasks(1000, clusterMaster);
+            clusterMaster.submitTask(new GetWebPageTask("https://www.facebook.com"));
+//            addEchoTasks(1000, clusterMaster);
         } catch (Throwable t) {
             log.error("Uncaught Exception", t);
         }
@@ -45,20 +47,6 @@ public class StartTestMaster {
         for(int i=0; i < size; i++) {
             clusterMaster.submitTask(new EchoTask(i));
         }
-    }
-
-    private static void readInput() throws Exception {
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(isr);
-        String line = null;
-/*
-        while ((line = reader.readLine()) != null) {
-            if() {
-
-            }
-            // Process line
-        }
-*/
     }
 
 }
